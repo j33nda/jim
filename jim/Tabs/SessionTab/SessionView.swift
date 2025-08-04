1215a6c5c46c09b8cc4107facecb5ea4e4dc61f8
Jan Halas <jan.halas@email.cz>
2025-08-04 09:56:53 +0200

    addExerciseButton moved to a variable

--- jim/Tabs/SessionTab/SessionView.swift
+++ jim/Tabs/SessionTab/SessionView.swift
@@ -6,38 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SessionView: View {
     
-    var session: Session
+    @Query var exerciseTypes: [ExerciseType]
     
+    @State var newExerciseSheet: Bool = false
+    
+    var session: Session
+
     
     var body: some View {
         VStack {
             
             ScrollView {
                 
-                ZStack {
-                    
-                    RoundedRectangle(cornerRadius: 10.0)
-                        .fill(
-                            MeshGradient(width: 2, height: 2, points: [
-                                .init(0, 0), .init(1, 0),
-                                .init(0, 1), .init(1, 1)
-                            ], colors: [
-                                .mint, .blueBackground,
-                                .blueBackground, .mint
-                            ], smoothsColors: true)
-                        )
-                    
-                    Text("Add Exercise")
-                        .foregroundStyle(Color.black)
-                        .bold()
-                        .font(.headline)
-
-                }
-                .frame(height: 50)
-                .padding()
+                addExerciseButton
+                    .onTapGesture {
+                        newExerciseSheet.toggle()
+                    }
                 
                 ForEach(session.exercises) { exercise in
                     
@@ -49,7 +37,43 @@ struct SessionView: View {
         .padding()
         .navigationTitle("Thu, 17. 3.")
         .navigationBarTitleDisplayMode(.large)
+        .sheet(isPresented: $newExerciseSheet) {
+            List(exerciseTypes) { excType in
+                Text(excType.name)
+                    .onTapGesture {
+                        let newExercise = Exercise(type: excType)
+                        session.exercises.append(newExercise)
+                        newExerciseSheet = false
+                    }
+            }
+        }
         
+    }
+    
+    
+    var addExerciseButton: some View {
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(
+                    MeshGradient(width: 2, height: 2, points: [
+                        .init(0, 0), .init(1, 0),
+                        .init(0, 1), .init(1, 1)
+                    ], colors: [
+                        .mint, .blueBackground,
+                        .blueBackground, .mint
+                    ], smoothsColors: true)
+                )
+            
+            Text("Add Exercise")
+                .foregroundStyle(Color.black)
+                .bold()
+                .font(.headline)
+
+        }
+        .frame(height: 50)
+        .padding()
     }
 }
 

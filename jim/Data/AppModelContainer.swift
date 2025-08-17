02130661e81b2849f32220c75b50f5d8d3b3b5f3
Jan Halas <jan.halas@email.cz>
2025-08-17 14:00:49 +0200

    Added AppModelContainer and exsTypes

---
+++ jim/Data/AppModelContainer.swift
@@ -0,0 +1,8 @@
+//
+//  AppContext.swift
+//  jim
+//
+//  Created by jenda on 17.08.2025.
+//
+
+import Foundation
---
+++ jim/Data/exsTypes.swift
@@ -0,0 +1,12 @@
+//
+//  exs_names.swift
+//  jim
+//
+//  Created by jenda on 17.08.2025.
+//
+
+import Foundation
+
+let exsTypes: [ExerciseType] = [
+    .init(name: "", unit: <#T##String#>)
+]
